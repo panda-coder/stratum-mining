49c431531cf844af7561bb20b7f3e3f7e53eb971
Ercy Moreira Neto <ercym.neto@gmail.com>
2025-06-13 16:17:36 -0300

    feat(template-provider): wallet creation

--- .docker/template-provider/start.sh
+++ .docker/template-provider/start.sh
@@ -7,11 +7,14 @@ bitcoind_pid=$! # Capture the PID of the backgrounded bitcoind process
 # Wait for bitcoind to start (adjust sleep time as needed)
 sleep 5
 
-# Create the wallet
-/app/bitcoin-sv2/bin/bitcoin-cli -regtest -rpcport=18443 -rpcuser=username -rpcpassword=password createwallet "testwallet"
+if [ -d "/app/data/regtest/wallets/testwallet" ]; then
+    echo "Directory exists"
+else
+    /app/bitcoin-sv2/bin/bitcoin-cli -regtest -rpcport=18443 -rpcuser=username -rpcpassword=password createwallet "testwallet"
+    /app/bitcoin-sv2/bin/bitcoin-cli -regtest  -rpcport=18443 -rpcuser=username -rpcpassword=password -generate 100
+fi
 
-# Generate blocks
-/app/bitcoin-sv2/bin/bitcoin-cli -regtest  -rpcport=18443 -rpcuser=username -rpcpassword=password -generate 100
+#/app/bitcoin-sv2/bin/bitcoin-cli -regtest -rpcport=18443 -rpcuser=username -rpcpassword=password loadwallet "testwallet"
 
 # Wait for bitcoind to exit (optional)
 wait $bitcoind_pid
